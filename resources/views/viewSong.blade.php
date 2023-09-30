f592c58e4c0e37ed8874a082be369c78c98188d2
Krzysztof Stanek <krzysztof.stanek@prografix.pl>
2023-09-30 21:28:24 +0200

    viewSong poprawki wyswietlania

--- resources/views/viewSong.blade.php
+++ resources/views/viewSong.blade.php
@@ -13,9 +13,29 @@
 <body>
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="#">Nazwa Strony</a>
-        </div>
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Nazwa zespołu</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Tekst</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Informacje o piosence</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">QR kod zespołu</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Tryb wyświetlania</a>
+                  </li>
+              </ul>
+            </div>
+          </div>
+
     </nav>
 
     <!-- Główna treść strony -->
@@ -46,9 +66,14 @@
 
     <!-- Przyciski przewijania -->
     <div class="scroll-button">
-        <button class="btn btn-primary" onclick="scrollToTop()"><</button>
-        <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#songModal">...</button>
-        <button class="btn btn-primary" onclick="scrollToBottom()">></button>
+        <div class="row">
+            <div class="col-4"><button class="btn btn-primary" onclick="scrollToTop()"><</button></div>
+            <div class="col-4"><button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#songModal">...</button></div>
+            <div class="col-4"><button class="btn btn-primary" onclick="scrollToBottom()">></button></div>
+        </div>
+
+
+
     </div>
 
     <!-- Modal z wyborem piosenek -->
